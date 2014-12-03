80308841682f101286f0f5391d3428ec2c16b310
p4welo <p4welo@gmail.com>
2014-12-03 23:47:38 +0100

    bootstrap

--- app/app.js
+++ app/app.js
@@ -1,27 +1,55 @@
 angular.module("christmas", ["firebase"])
 
-    .controller("indexController", function ($scope, $firebase) {
+    .controller("indexController", function ($scope, firebaseFactory) {
 
-        $scope.loading = true;
-        $scope.newMessageText = "";
-
-        function initChat() {
-            var ref = new Firebase("https://shining-fire-1146.firebaseio.com/messages");
-
-            ref.on("value", function (snapshot) {
-                console.log(snapshot.val());
-                $scope.loading = false;
-            }, function (errorObject) {
-                console.log("The read failed: " + errorObject.code);
+        $scope.messages = firebaseFactory.allMessages();
+        $scope.people = firebaseFactory.allPeople();
+        $scope.newPerson = "";
+        $scope.selected;
+        $scope.select = function (person) {
+            if ($scope.selected == person) {
+                $scope.selected = null;
+                return;
+            }
+            $scope.selected = person;
+        }
+        $scope.addPerson = function (e) {
+            if (e.keyCode != 13) return;
+            firebaseFactory.addPerson({
+                name: $scope.newPerson
             });
+            $scope.newPerson = "";
 
-            var sync = $firebase(ref);
-            return sync.$asArray();
         }
+        $scope.delPerson = function (person) {
+            firebaseFactory.delPerson(person);
+        }
+    })
 
-        $scope.messages = initChat();
-        $scope.addMessage = function (text) {
-            $scope.newMessageText = "";
-            $scope.messages.$add({text: text, date: new Date()});
-        }
+    .factory("firebaseFactory", function ($firebase) {
+        var ref = new Firebase("https://shining-fire-1146.firebaseio.com");
+        var messages = $firebase(ref.child('messages')).$asArray();
+        var people = $firebase(ref.child('people')).$asArray();
+
+        var Fire = {
+            allMessages: function () {
+                return messages;
+            },
+            addMessage: function (message) {
+                return messages.$add(message);
+            },
+            delMessage: function (message) {
+                return messages.$remove(message);
+            },
+            allPeople: function () {
+                return people;
+            },
+            addPerson: function (person) {
+                return people.$add(person);
+            },
+            delPerson: function (person) {
+                return people.$remove(person);
+            }
+        };
+        return Fire;
     })
