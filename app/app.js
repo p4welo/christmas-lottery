457769d6703685aaf2ba9610058bb35fc0f37448
p4welo <p4welo@gmail.com>
2014-12-03 12:51:28 +0100

    bootstrap

--- app/app.js
+++ app/app.js
@@ -1,14 +1,27 @@
 angular.module("christmas", ["firebase"])
 
-.controller("indexController", function ($scope, $firebase) {
+    .controller("indexController", function ($scope, $firebase) {
+
+        $scope.loading = true;
+        $scope.newMessageText = "";
 
         function initChat() {
             var ref = new Firebase("https://shining-fire-1146.firebaseio.com/messages");
+
+            ref.on("value", function (snapshot) {
+                console.log(snapshot.val());
+                $scope.loading = false;
+            }, function (errorObject) {
+                console.log("The read failed: " + errorObject.code);
+            });
+
             var sync = $firebase(ref);
             return sync.$asArray();
         }
+
         $scope.messages = initChat();
-        $scope.addMessage = function(text) {
+        $scope.addMessage = function (text) {
             $scope.messages.$add({text: text});
+            $scope.newMessageText = "";
         }
     })
